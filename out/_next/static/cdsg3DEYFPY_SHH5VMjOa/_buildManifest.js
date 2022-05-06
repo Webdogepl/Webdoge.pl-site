d2adadd98cc903e24788b473bb3c3531cd79e3bb
Webdgepl <cholewski185@gmail.com>
2022-05-07 00:40:19 +0200

    Disable intro animation

---
+++ out/_next/static/cdsg3DEYFPY_SHH5VMjOa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8e9b7a80d801cbbd.js"],"/_error":["static/chunks/pages/_error-f921be93e37df70c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ function App() {
 			<motion.main
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
-				transition={{ duration: 2, delay: 1 }}
+				transition={{ duration: 0, delay: 0 }}
 			>
 				<Header />
 				<Portfolio />
